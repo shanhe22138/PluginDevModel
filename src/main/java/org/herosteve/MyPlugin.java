492fe236dadc3307fe60287c6fd58ac298060128
2451125842 <2451125842@qq.com>
2021-07-03 15:52:55 +0800

    code: complete model

---
+++ src/main/java/org/herosteve/MyPlugin.java
@@ -0,0 +1,17 @@
+package org.herosteve;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author shanhe
+ * @className MyPlugin
+ * @date 2021-07-03 15:45
+ **/
+public class MyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+}
---
+++ src/test/java/org/herosteve/MyPluginTest.java
@@ -0,0 +1,20 @@
+package org.herosteve;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyPluginTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
