f499af06828d0515ce907f8bd1649859097e6e3b
2451125842 <2451125842@qq.com>
2021-07-03 16:20:46 +0800

    finish: version1.0-snapshot

---
+++ src/main/java/org/herosteve/MyPlugin.java
@@ -0,0 +1,17 @@
+package org.herosteve;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author shanhe
+ * @className MyPlugin
+ * @date 2021-07-03 16:14
+ **/
+public class MyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+}
---
+++ src/test/java/org/herosteve/MyPluginTest.java
@@ -0,0 +1,19 @@
+package org.herosteve;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author shanhe
+ * @className MyPluginTest
+ * @date 2021-07-03 16:18
+ **/
+public class MyPluginTest {
+
+    @Test
+    public void HelloWorld() {
+        System.out.println("hello world!");
+    }
+
+}
